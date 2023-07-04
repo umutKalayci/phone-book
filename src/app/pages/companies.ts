5a5a5086fa2358cc31225d6c5ab5e732c349c665
Umut Kalayci <kalayciumut@windowslive.com>
2023-07-04 18:48:16 +0300

    chore: local data added for companies and persons.

---
+++ src/app/pages/companies.ts
@@ -0,0 +1,44 @@
+export interface Company {
+  id: String;
+  name: String;
+  phoneNumber: String;
+  image: String;
+}
+export const companies = [
+  {
+    id: '1',
+    name: 'Company 1',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '2',
+    name: 'Company 2',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '3',
+    name: 'Company 3',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '4',
+    name: 'Company 4',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '5',
+    name: 'Company 5',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '6',
+    name: 'Company 6',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+];
---
+++ src/app/pages/persons.ts
@@ -0,0 +1,76 @@
+export interface Person {
+  id: String;
+  name: String;
+  phoneNumber: String;
+  image: String;
+  companies?: String[];
+}
+export const persons = [
+  {
+    id: '1',
+    name: 'Person 1',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    companies: ['1', '3'],
+  },
+  {
+    id: '2',
+    name: 'Person 2',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '3',
+    name: 'Person 3',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '4',
+    name: 'Person 4',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '5',
+    name: 'Person 5',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '6',
+    name: 'Person 6',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '7',
+    name: 'Person 7',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '8',
+    name: 'Person 8',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '9',
+    name: 'Person 9',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '10',
+    name: 'Person 10',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+  {
+    id: '11',
+    name: 'Person 11',
+    phoneNumber: '05444444444',
+    image: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+  },
+];
