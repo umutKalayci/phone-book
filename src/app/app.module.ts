1a07d3ce65c54c9a4c96f35f1283d5b200996ede
Umut Kalayci <kalayciumut@windowslive.com>
2023-07-04 10:48:08 +0300

    feat: Routing added with (navbar, calls, contacts) components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CallsComponent } from './pages/calls/calls.component';
 
+const appRoute: Routes = [
+  { path: '', redirectTo: '/contacts', pathMatch: 'full' },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'calls', component: CallsComponent },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ContactsComponent,
+    CallsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    RouterModule.forRoot(appRoute),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
