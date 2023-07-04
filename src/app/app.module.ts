92786dbeaebc87247e5424745048aa5641d43137
Umut Kalayci <kalayciumut@windowslive.com>
2023-07-04 18:48:44 +0300

    feat: Contacts page list and edit persons.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ContactsComponent } from './pages/contacts/contacts.component';
 import { CallsComponent } from './pages/calls/calls.component';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
 const appRoute: Routes = [
   { path: '', redirectTo: '/contacts', pathMatch: 'full' },
   { path: 'contacts', component: ContactsComponent },
+  { path: 'contacts/:id', component: ContactsComponent },
   { path: 'calls', component: CallsComponent },
 ];
 @NgModule({
@@ -27,11 +36,20 @@ const appRoute: Routes = [
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(appRoute),
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatIconModule,
-    BrowserAnimationsModule,
+    MatGridListModule,
     MatButtonModule,
-    RouterModule.forRoot(appRoute),
+    MatCardModule,
+    MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatExpansionModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
-
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { Person, persons } from '../persons';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Company, companies } from '../companies';
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.scss']
+  styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent {
+  persons: Person[] = persons;
+  companies: Company[] = companies;
+  _selectedPerson!: Person | null;
+  get selectedPerson() {
+    return this._selectedPerson;
+  }
+  set selectedPerson(value) {
+    this._selectedPerson = value;
+    this.personCompanies = [];
+    this._selectedPerson?.companies?.forEach((selectedCompanyID) => {
+      let company = companies.find((comp) => comp.id == selectedCompanyID);
+      if (company) this.personCompanies?.push(company);
+    });
+  }
 
+  personForm!: FormGroup | null;
+  personCompanies!: Company[] | null;
+  constructor(private route: ActivatedRoute) {}
+  ngOnInit() {
+    this.route.paramMap.subscribe((params: ParamMap) => {
+      let personID = params.get('id');
+      if (personID) {
+        let selectedPerson = persons.find((person) => person.id == personID);
+        if (selectedPerson) {
+          this.selectedPerson = selectedPerson;
+          this.personForm = new FormGroup({
+            image: new FormControl(selectedPerson?.image),
+            name: new FormControl(selectedPerson?.name),
+            phone: new FormControl(selectedPerson?.phoneNumber),
+          });
+        }
+      }
+    });
+  }
+  savePerson() {
+    console.log(this.personForm?.value);
+  }
+  deletePerson() {
+    console.log(this.selectedPerson);
+  }
+
+  addPersonCompany() {
+    this.personCompanies?.push(companies[0]);
+  }
+  removeCompanyField(companyID: String) {
+    console.log(companyID);
+    this.personCompanies?.splice(
+      this.personCompanies.findIndex((x) => x.id == companyID),
+      1
+    );
+  }
 }
