d05ca9dc208499fc6b236c6e391e3103fea2991d
Umut Kalayci <kalayciumut@windowslive.com>
2023-07-05 16:57:56 +0300

    feat: Calls page

---
+++ src/app/pages/calls.ts
@@ -0,0 +1,39 @@
+import { Person, persons } from './persons';
+
+export interface Call {
+  id: String;
+  caller: Person;
+  callee: Person;
+  date: Date;
+  duration: Number;
+}
+export const calls: Call[] = [
+  {
+    id: '1',
+    caller: persons[0],
+    callee: persons[1],
+    date: new Date(),
+    duration: 100,
+  },
+  {
+    id: '2',
+    caller: persons[1],
+    callee: persons[0],
+    date: new Date('07-05-2023'),
+    duration: 0,
+  },
+  {
+    id: '3',
+    caller: persons[1],
+    callee: persons[0],
+    date: new Date('07-04-2023'),
+    duration: 100,
+  },
+  {
+    id: '4',
+    caller: persons[0],
+    callee: persons[1],
+    date: new Date('05-14-2023'),
+    duration: 0,
+  },
+];
--- src/app/pages/calls/calls.component.ts
+++ src/app/pages/calls/calls.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Person, persons } from '../persons';
+import { Call, calls } from '../calls';
 
 @Component({
   selector: 'app-calls',
   templateUrl: './calls.component.html',
-  styleUrls: ['./calls.component.scss']
+  styleUrls: ['./calls.component.scss'],
 })
 export class CallsComponent {
-
+  personID = '1';
+  todayDate = new Date();
+  yesterdayDate = new Date();
+  persons: Person[] = persons;
+  calls: Call[] = calls;
+  constructor() {
+    console.log(calls);
+    this.yesterdayDate.setDate(this.yesterdayDate.getDate() - 1);
+  }
 }
