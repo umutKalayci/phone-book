883eb39946079b0e338076e3a02819210cc79062
Umut Kalayci <kalayciumut@windowslive.com>
2023-07-06 09:55:24 +0300

    docs: dataset images change.

--- src/app/pages/calls.ts
+++ src/app/pages/calls.ts
@@ -17,10 +17,17 @@ export const calls: Call[] = [
   },
   {
     id: '2',
-    caller: persons[1],
+    caller: persons[2],
     callee: persons[0],
     date: new Date('07-05-2023'),
     duration: 0,
+  },
+  {
+    id: '5',
+    caller: persons[4],
+    callee: persons[0],
+    date: new Date('07-05-2023'),
+    duration: 100,
   },
   {
     id: '3',
@@ -32,7 +39,7 @@ export const calls: Call[] = [
   {
     id: '4',
     caller: persons[0],
-    callee: persons[1],
+    callee: persons[3],
     date: new Date('05-14-2023'),
     duration: 0,
   },
