31028c42a697f3d6900afd31b409e75730fa2efa
Umut Kalayci <kalayciumut@windowslive.com>
2023-07-20 09:17:32 +0300

    build

--- src/enviroments/enviroment.ts
+++ src/enviroments/enviroment.ts
@@ -1,7 +1,7 @@
 export const enviroment = {
   production: false,
   config: {
-    apiUrl: 'https://3df9-176-232-60-101.ngrok-free.app/',
+    apiUrl: 'https://b9bf-176-232-60-101.ngrok-free.app/',
     // apiUrl: 'http://127.0.0.1:8000/',
   },
 };
